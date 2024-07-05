025c8c072c600c44a310641f1bd88b8ea157fccb
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-05 19:22:03 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ JavaScript/프로그래머스/0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,7 +1,9 @@
-function solution(slice, n) {
-    let answer = Math.floor(n/slice);
-    if (n % slice) {
-        answer ++;
-    }
-    return answer;
-}
+// function solution(slice, n) {
+//     let answer = Math.floor(n/slice);
+//     if (n % slice) {
+//         answer ++;
+//     }
+//     return answer;
+// }
+
+const solution = (slice, n) => Math.ceil(n / slice);
