26b55fb570ea761ddacd839f6adcebe2a5958688
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-06 18:30:39 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ JavaScript/프로그래머스/0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,17 +1,42 @@
+// function solution(num_list) {
+//     let even = 0;
+//     let odd = 0;
+//     let answer = [];
+    
+//     for (let i = 0; i < num_list.length; i++) {
+//         if (num_list[i] % 2 === 0) {
+//             even ++;
+//         } else {
+//             odd ++;
+//         }
+//     }
+    
+//     answer.push(even, odd);
+    
+//     return answer;
+// }
+
+
+// function solution(num_list) {
+//     let answer = [0, 0];
+    
+//     for (a of num_list) {
+//         answer[a % 2] += 1;
+//     }
+    
+//     return answer;
+// }
+
+// function solution(num_list) {
+//     return [num_list.filter(num => num % 2 === 0).length,
+//            num_list.filter(num => num % 2 !== 0).length]
+// }
+
+// const solution = (num_list) => [num_list.filter(num => num % 2 === 0).length,
+//                                num_list.filter(num => num % 2 !== 0).length]
+
 function solution(num_list) {
-    let even = 0;
-    let odd = 0;
-    let answer = [];
-    
-    for (let i = 0; i < num_list.length; i++) {
-        if (num_list[i] % 2 === 0) {
-            even ++;
-        } else {
-            odd ++;
-        }
-    }
-    
-    answer.push(even, odd);
-    
+    let answer = [0, 0];
+    for (a of num_list) answer[a % 2] ++;
     return answer;
 }
