e1c1e58c3f52aafbd6acfdf980d59a0dc78d1a0a
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-06 17:48:55 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -0,0 +1,11 @@
+function solution(my_string, letter) {
+    let array = [];
+    
+    for (let i = 0; i <= my_string.length; i++) {
+        if (my_string[i] !== letter) {
+            array.push(my_string[i]);
+        }
+    }
+    
+    return array.join('');
+}
