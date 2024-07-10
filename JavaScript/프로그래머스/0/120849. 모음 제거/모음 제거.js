272670663b8e4d3ef46d37ec31833119b6a3109e
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-10 18:31:14 +0900

    [level 0] Title: 모음 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120849. 모음 제거/모음 제거.js
+++ JavaScript/프로그래머스/0/120849. 모음 제거/모음 제거.js
@@ -1,5 +1,28 @@
-function solution(my_string) {
-    const vowel = ["a", "e", "i", "o", "u"]
+// function solution(my_string) {
+//     const vowel = ["a", "e", "i", "o", "u"]
     
-    return my_string.split("").filter(char => !vowel.includes(char)).join("")
+//     return my_string.split("").filter(char => !vowel.includes(char)).join("")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solution(my_string) {
+    return my_string
+        .split("")
+        .filter(x => !["a", "e", "i", "o", "u"].includes(x))
+        .join("")
 }
