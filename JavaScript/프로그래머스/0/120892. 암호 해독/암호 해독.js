f704777b129ab69101b3519f9ff92779d658cae2
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-10 19:03:53 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120892. 암호 해독/암호 해독.js
+++ JavaScript/프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -1,9 +1,32 @@
-function solution(cipher, code) {
-    let answer = ""
+// function solution(cipher, code) {
+//     let answer = ""
     
-    for (let i = code -1; i < cipher.length; i += code) {
-        answer += cipher[i]
+//     for (let i = code -1; i < cipher.length; i += code) {
+//         answer += cipher[i]
+//     }
+    
+//     return answer;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function solution(cipher, code) {
+    let answer = [];
+    
+    for (let i = code-1; i < cipher.length; i += code) {
+        answer.push(cipher[i])
     }
     
-    return answer;
+    return answer.join("");
 }
