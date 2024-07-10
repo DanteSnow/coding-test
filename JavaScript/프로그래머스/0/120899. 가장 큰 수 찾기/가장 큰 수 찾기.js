98ae840cd7eb2075ddf7852e0d91b7e44d11d998
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-10 19:13:45 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ JavaScript/프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,13 +1,34 @@
-function solution(array) {
-    let maxNumber = -1;
-    let maxIndex = -1;
+// // function solution(array) {
+// //     let maxNumber = -1;
+// //     let maxIndex = -1;
     
-    for (let i = 0; i < array.length; i++) {
-        if (array[i] > maxNumber) {
-            maxNumber = array[i];
-            maxIndex = i;
-        }
-    }
+// //     for (let i = 0; i < array.length; i++) {
+// //         if (array[i] > maxNumber) {
+// //             maxNumber = array[i];
+// //             maxIndex = i;
+// //         }
+// //     }
     
-    return [maxNumber, maxIndex]
-}
+// //     return [maxNumber, maxIndex]
+// // }
+
+// function solution(array) {
+//     return [Math.max(...array), array.indexOf(Math.max(...array))];
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+const solution = (array) => [Math.max(...array), array.indexOf(Math.max(...array))]
+
+
+
