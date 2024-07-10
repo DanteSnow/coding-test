8df00602e49c6bc58c212de56ca023d1353d4bab
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-10 18:28:14 +0900

    [level 0] Title: 개미 군단, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
+++ JavaScript/프로그래머스/0/120837. 개미 군단/개미 군단.js
@@ -1,10 +1,38 @@
+// function solution(hp) {
+//     let answer = 0;
+    
+//     answer += Math.floor(hp / 5);
+//     hp %= 5;
+    
+//     answer += Math.floor(hp / 3);
+//     hp %= 3;
+    
+//     answer += hp;
+    
+//     return answer;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function solution(hp) {
     let answer = 0;
     
-    answer += Math.floor(hp / 5);
+    answer += Math.floor(hp / 5)
     hp %= 5;
     
-    answer += Math.floor(hp / 3);
+    answer += Math.floor(hp / 3)
     hp %= 3;
     
     answer += hp;
