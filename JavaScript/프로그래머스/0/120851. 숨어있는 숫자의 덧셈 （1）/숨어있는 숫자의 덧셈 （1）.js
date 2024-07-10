a608583ae14f91c2f3b83c0c6f91e9d0a2d0dfc6
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-10 15:58:42 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
+++ JavaScript/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.js
@@ -1,8 +1,18 @@
+// function solution(my_string) {
+//     return my_string
+//         .split("")
+//         .filter(x => ["1","2","3","4","5","6","7","8","9","0"]
+//         .includes(x))
+//         .map(Number)
+//         .reduce((a,b) => a+b);
+// }
+
 function solution(my_string) {
-    return my_string
-        .split("")
-        .filter(x => ["1","2","3","4","5","6","7","8","9","0"]
-        .includes(x))
-        .map(Number)
-        .reduce((a,b) => a+b);
+    const numbers = my_string.match(/\d/g);
+    
+    if (!numbers) {
+        return
+    }
+    
+    return numbers.map(Number).reduce((a, b) => a + b);
 }
