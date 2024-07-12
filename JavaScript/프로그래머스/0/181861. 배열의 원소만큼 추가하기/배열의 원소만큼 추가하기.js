d955740a5b9069fa617db2ef30211721a6d1c85f
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-12 16:06:57 +0900

    [level 0] Title: 배열의 원소만큼 추가하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181861. 배열의 원소만큼 추가하기/배열의 원소만큼 추가하기.js
@@ -0,0 +1,11 @@
+function solution(arr) {
+    let answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 1; j <= arr[i]; j++) {
+            answer.push(arr[i]);
+        }
+    }
+    
+    return answer;
+}
