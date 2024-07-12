0ada3e7346530af22be28cc7e0701ab709830585
ClydeHan <150772344+DanteSnow@users.noreply.github.com>
2024-07-12 16:46:33 +0900

    [level 0] Title: rny_string, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181863. rny＿string/rny＿string.js
+++ JavaScript/프로그래머스/0/181863. rny＿string/rny＿string.js
@@ -1,1 +1,16 @@
-const solution = (rny_string) => rny_string.replaceAll("m", "rn");
+// const solution = (rny_string) => rny_string.replaceAll("m", "rn");
+
+function solution(rny_string) {
+    let answer = [];
+    const prmArray = rny_string.split("");
+    
+    for (let i = 0; i < prmArray.length; i++) {
+        if (prmArray[i] === "m") {
+            answer.push("r","n")
+        } else {
+            answer.push(prmArray[i]);
+        }
+    }
+    
+    return answer.join("");
+}
